14fba5c6ecd8ac5f4140b2249c1e21c961f5b972
Nino Škopac <nino.skopac@gmail.com>
2016-07-15 23:53:23 +0200

    List() with keys

---
+++ list-keys.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+// https://wiki.php.net/rfc/list_keys
+
+class NationalFoods {
+    private $foods;
+    
+    public function __construct(array $nationalFoods) {
+        // this ...
+        list(
+            'thailand' => $this->foods['thailand'],
+            'uk' => $this->foods['uk'],
+            'usa' => $this->foods['usa']
+        ) = $nationalFoods;
+    }
+    
+    public function __destruct() {
+        print_r($this->foods);
+    }
+}
+
+new NationalFoods(
+    [
+        'thailand' => 'Pad Thai',
+        'uk' => 'Fish and Chips',
+        'usa' => 'Burgers'
+    ]
+);
---
+++ short-list-syntax.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+
+// https://wiki.php.net/rfc/short_list_syntax
+
